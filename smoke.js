38d7f6fd27b67cca29d77b47fb799314e323d61f
Kenan Sulayman <kenan@sly.mn>
2016-09-08 01:09:53 +0200

    Add more smoke coverage

--- smoke.js
+++ smoke.js
@@ -14,15 +14,22 @@ const sysdigClient = sysdig.transport(sysdigHttpTransport).config({
 });
 
 Promise.coroutine(function* () {
+    if (process.argv.length === 3) {
+        return console.log(yield sysdigClient[process.argv[2]]());
+    }
+
     assert('username' in (yield sysdigClient.getUserInfo()));
 
     assert((yield sysdigClient.getConnectedAgents()).constructor === Array);
     assert((yield sysdigClient.getAlerts()).constructor === Array);
 
+    assert((yield sysdigClient.getExploreGroupingHierarchy()).constructor === Array);
+
+    assert((yield sysdigClient.getExploreGroupingHierarchy()).constructor === Array);
+
     /* None of them work right now: */
 
     //assert((yield sysdigClient.getNotifications()).constructor === Array);
-    //console.log(yield sysdigClient.getExploreGroupingHierarchy())
-    //console.log(yield sysdigClient.getDataRetentionInfo())
+    console.log(yield sysdigClient.getDataRetentionInfo())
     //console.log(yield sysdigClient.getDashboards())
 })();
